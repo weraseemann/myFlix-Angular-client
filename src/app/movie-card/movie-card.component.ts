5b9d9a0d62c966fe157292f4e57402e8578d5bc3
weraseemann <wera.seemann@gmail.com>
2024-09-20 10:46:45 +0200

    Added MatIconModule

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -11,6 +11,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-card',
@@ -29,6 +30,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatSnackBarModule,
     HttpClientModule,
+    MatIconModule
 
   ],
 

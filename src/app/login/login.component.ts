512f5073dcccb7af9e34f591a66b4d49a03042a7
weraseemann <wera.seemann@gmail.com>
2025-07-04 13:14:35 +0200

    deleted some comments

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-// You'll use this import to close the dialog on success
+// use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
 
-// This import brings in the API calls we created in 6.2
+// This import brings in the API calls 
 import { FetchApiDataService } from '../fetch-api-data.service';
 
 // This import is used to display notifications back to the user
